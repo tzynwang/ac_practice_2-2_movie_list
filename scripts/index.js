efabd67b3955ed507d134a36950b2fceab8f3b3b
tzyn (Windows Fork) <tzyn.wang@gmail.com>
2021-05-27 15:37:00 +0800

    feat: add movie-search feature

--- scripts/index.js
+++ scripts/index.js
@@ -5,5 +5,14 @@ interaction.loadIndexPageContents(300)
 
 main.elementObject.movieCardsSection.addEventListener('click', interaction.movieCardInteract)
 main.elementObject.pagination.addEventListener('click', event => {
-  interaction.paginationInteract(event, 'index')
+  interaction.paginationInteract(event, main.config.pageStatus)
+})
+
+main.elementObject.searchButton.addEventListener('click', () => {
+  const userInput = main.elementObject.searchInput.value
+  interaction.searchMovieByTitle(userInput)
+})
+
+main.elementObject.searchInput.addEventListener('keypress', event => {
+  if (event.keyCode === 13) main.elementObject.searchButton.click()
 })
